d240e3b0569fec3bfbef83390bdaa31472f5ec88
tang.ting <tangtingsg@gmail.com>
2021-03-13 17:02:04 +0800

    add subarray o(n) way

--- Array/subarray-sum.ts
+++ Array/subarray-sum.ts
@@ -31,3 +31,31 @@ console.log(subarraySum([1, 1, 1], 2) === 2);
 console.log(subarraySum([1, 2, 3], 2) === 1);
 console.log(subarraySum([1], 0) === 0);
 console.log(subarraySum([1, -1, 0], 0) === 3);
+console.log(subarraySum([1,2,1,2,1], 3) === 4);
+console.log(subarraySum([0,0,0,0,0,0], 0) === 21);
+
+
+// 求 sum(i, j)的值为k的i，j值
+// sum(i, j) = sum(0, j) - sum(0 , i) = k => sum(0, i) = sum(0 , j) - k
+var subarraySum2 = function(nums, k) {
+  const sumjMap = new Map();
+  sumjMap.set(0 , 1)
+  let sumj = 0;
+  let count = 0;
+  for (const num of nums) {
+    sumj += num;
+    const sumCount = sumjMap.get(sumj) || 0;
+    if (sumjMap.has(sumj - k)) {
+      count += sumjMap.get(sumj - k);
+    }
+    sumjMap.set(sumj, sumCount + 1)
+  }
+  return count;
+}
+
+console.log(subarraySum2([1, 1, 1], 2) === 2);
+console.log(subarraySum2([1, 2, 3], 2) === 1);
+console.log(subarraySum2([1], 0) === 0);
+console.log(subarraySum2([1, -1, 0], 0) === 3);
+console.log(subarraySum2([1, 2, 1, 2, 1], 3) === 4);
+console.log(subarraySum2([0, 0, 0, 0, 0, 0], 0) === 21);
